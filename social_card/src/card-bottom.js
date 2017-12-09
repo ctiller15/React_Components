0ada75c029cc7b1c6e734be335fb8d14b26a3669
ctiller15 <christiller2015@gmail.com>
2017-12-09 15:59:05 -0500

    update with colors, hover animations and transitions

--- social_card/src/card-bottom.js
+++ social_card/src/card-bottom.js
@@ -10,18 +10,41 @@ class CardBottom extends React.Component{
 		}
 	}
 
-	increment(stateVar, varName) {
-		console.log(stateVar, varName);
+	componentDidMount(){
+		this.refs.socialButtons.childNodes.forEach((item) => {
+			console.log(item.childNodes[1]);
+			item.childNodes[1].addEventListener("animationend", (e) => {
+				console.log("animation ended!");
+				console.log(item.childNodes[1].classList);
+				item.childNodes[1].classList.remove("animated");
+			});
+		});
+	}
+
+	componentWillUpdate(nextProps, nextState){
+		console.log("State changed!", nextProps, nextState);
+	}
+
+	increment(event, stateVar, varName) {
+		// event.target.classList.toggle("animated");
+		var eventItem = event.currentTarget.childNodes[1];
+		// eventItem.addEventListener("animationEnd", () => {
+		// 	console.log("Animation ended!");
+		// })
+		console.log(eventItem);
+		console.log(eventItem.classList);
+		eventItem.classList.add("animated");
+		// console.log(event.target, stateVar, varName);
 		this.setState({[varName]: stateVar + 1});
 	}
 
 	render() {
 		return(
 			<div className="card-bottom">
-				<ul className="social">
-					<li onClick={() => this.increment(this.state.likes, "likes")}><i className="fa fa-thumbs-o-up" aria-hidden="true"></i><span className="count">{this.state.likes}</span></li>
-					<li onClick={() => this.increment(this.state.comments, "comments")}><i className="fa fa-comment" aria-hidden="true" ></i><span className="count">{this.state.comments}</span></li>
-					<li onClick={() => this.increment(this.state.subscriptions, "subscriptions")}><i className="fa fa-share-alt" aria-hidden="true" ></i><span className="count">{this.state.subscriptions}</span></li>
+				<ul className="social" ref={"socialButtons"}>
+					<li ref={"socialButton"} onClick={(e) => this.increment(e, this.state.likes, "likes")}><i className="fa fa-thumbs-o-up" aria-hidden="true"></i><span className="count">{this.state.likes}</span></li>
+					<li ref={"socialButton"} onClick={(e) => this.increment(e, this.state.comments, "comments")}><i className="fa fa-comment-o" aria-hidden="true" ></i><span className="count">{this.state.comments}</span></li>
+					<li ref={"socialButton"} onClick={(e) => this.increment(e, this.state.subscriptions, "subscriptions")}><i className="fa fa-share-alt" aria-hidden="true" ></i><span className="count">{this.state.subscriptions}</span></li>
 				</ul>
 			</div>
 		);
--- social_card/src/card-main.js
+++ social_card/src/card-main.js
@@ -3,12 +3,23 @@ import React from 'react';
 function CardMain(props){
 	const bgImage = require(`${props.info.bgImage}`);
 
+	var style;
+
+	if(props.info.previewTitle){
+		console.log(props.info.previewTitle);
+		style = {
+			borderLeft: "5px solid var(--borders)"
+		}
+	} else{
+		style = null;
+	}
+
 	return(
 		<div className="card-main">
 			<div className="card-image-bg" style={{backgroundImage: `url(${bgImage})`}}>
 				{/*<img src="#" alt="This is the thing we're talking about!"></img>*/}
 			</div>
-			<div className="card-preview-text">
+			<div className="card-preview-text" style={style}>
 				<h3>{props.info.previewTitle}</h3>
 				<p>{props.info.previewText}</p>
 			</div>
