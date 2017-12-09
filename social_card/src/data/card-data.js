d0765e717322fef18331b7673315faee495c67bf
ctiller15 <christiller2015@gmail.com>
2017-12-08 21:43:45 -0500

    create separate data file and refactor to take in from external data source.

---
+++ social_card/src/data/card-data.js
@@ -0,0 +1,20 @@
+var cardData = [{
+				topProps: {
+					authorName: "Matt Nelson",
+					authorImg: "./images/matt-nelson.jpg",
+					authorLink: "https://unsplash.com/@mnelson",
+					authorText: "Dogs are great. Training them is even better."
+				},
+				mainProps: {
+					bgImage: "./images/matt-nelson-259365.jpg",
+					previewTitle: "Find a trainer near you!",
+					previewText: "Take a moment and search for the best, most effective dog trainers in your area."
+				},
+				socialProps: {
+					likes: 10,
+					comments: 25,
+					subscriptions: 8
+				}
+					},];
+
+export default cardData;
--- social_card/src/index.js
+++ social_card/src/index.js
@@ -3,35 +3,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import SocialCard from './card.js';
+import cardData from './data/card-data.js';
 
 // The board will contain all of the indiviual card components.
 class Board extends React.Component{
+	// This takes the individual card data and creates an array of JSX elements
+	cards(){
+		return cardData.map((data, ind) => {
+			return <SocialCard key={ind} cardProps={data}/>
+		});
+	}
+
 	render(){
 		// This will return the social card component.
 		return(
 			<div className="cardBoard">
-				<SocialCard
-					cardProps={
-						{
-							topProps: {
-								authorName: "Matt Nelson",
-								authorImg: "./images/matt-nelson.jpg",
-								authorLink: "https://unsplash.com/@mnelson",
-								authorText: "Dogs are great. Training them is even better."
-							},
-							mainProps: {
-								bgImage: "./images/matt-nelson-259365.jpg",
-								previewTitle: "Find a trainer near you!",
-								previewText: "Take a moment and search for the best, most effective dog trainers in your area."
-							},
-							socialProps: {
-								likes: 10,
-								comments: 25,
-								subscriptions: 8
-							}
-						}
-					}
-				/>
+				{this.cards()}
 			</div>
 		);
 	}
