c7974b40ce50c9cd82c44026ee50be1d5b7353d5
ctiller15 <christiller2015@gmail.com>
2017-12-08 23:08:33 -0500

    add more photos and data

--- social_card/src/data/card-data.js
+++ social_card/src/data/card-data.js
@@ -3,6 +3,7 @@ var cardData = [{
 					authorName: "Matt Nelson",
 					authorImg: "./images/matt-nelson.jpg",
 					authorLink: "https://unsplash.com/@mnelson",
+					authorSite: "Unsplash",
 					authorText: "Dogs are great. Training them is even better."
 				},
 				mainProps: {
@@ -14,6 +15,63 @@ var cardData = [{
 					likes: 10,
 					comments: 25,
 					subscriptions: 8
+				}
+					},
+				{
+				topProps: {
+					authorName: "Rachel Park",
+					authorImg: "./images/rachel-park.jpg",
+					authorLink: "https://unsplash.com/@therachelstory",
+					authorSite: "Unsplash",
+					authorText: "Breakfast is the most important meal of the day."
+				},
+				mainProps: {
+					bgImage: "./images/rachel-park-366508.jpg",
+					previewTitle: "101 Breakfast ideas",
+					previewText: "Every way possible to create your favorite breakfasts, archived."
+				},
+				socialProps: {
+					likes: 65,
+					comments: 10,
+					subscriptions: 87
+				}
+					},
+				{
+				topProps: {
+					authorName: "Christopher Tiller",
+					authorImg: "./images/christopher-tiller.jpg",
+					authorLink: "https://www.linkedin.com/in/christopher-tiller-9b853b129/",
+					authorSite: "LinkedIn",
+					authorText: "Hey guys, check out my blog post!"
+				},
+				mainProps: {
+					bgImage: "./images/matrix.jpg",
+					previewTitle: "My ReactJS journey",
+					previewText: "React seems really intimidating to anyone starting out. Here's how to build some momentum."
+				},
+				socialProps: {
+					likes: 18,
+					comments: 22,
+					subscriptions: 2
+				}
+					},
+				{
+				topProps: {
+					authorName: "Jessica",
+					authorImg: "./images/woman.jpg",
+					authorLink: "https://pixabay.com/en/users/Jessica001234-3652586/",
+					authorSite: "pixabay",
+					authorText: "After a long nine months of waiting, Jason is finally ready to come out and say hello to the world!"
+				},
+				mainProps: {
+					bgImage: "./images/baby.jpg",
+					previewTitle: "",
+					previewText: ""
+				},
+				socialProps: {
+					likes: 72,
+					comments: 40,
+					subscriptions: 0
 				}
 					},];
 
